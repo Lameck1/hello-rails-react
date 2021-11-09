753ff4ca0dfde05eecfc6092eaf2c6f7fd71893d
Lameck1 <lameck72@gmail.com>
2021-11-09 07:17:15 +0300

    Setup API controllers

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,13 @@
-class Api::MessagesController < ApplicationController
+class Api::MessagesController < Api::ApiController
+  before_action :set_message, only: %i[show update destroy]
+
+  def index
+    @messages = Message.all
+    render json: @messages
+  end
+
+  def show
+    @message = Message.find(params[:id])
+    render json: @message
+  end
 end
